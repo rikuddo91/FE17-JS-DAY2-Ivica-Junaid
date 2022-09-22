a6c9241838ce926a58dcff6618f837088e1a1998
rikuddo91 <gullkhan0@live.com>
2022-09-22 10:56:06 +0200

    BasicEx1

---
+++ Basic/script/script.js
@@ -0,0 +1,15 @@
+/* -------------------Exercise #1 ------------------------*/
+
+
+let temp = Math.floor(Math.random() * (26 + 5) - 5)
+
+if(temp<11){
+    console.log(`Temperature: ${temp}. The weather is cold`)
+}
+else {
+    console.log(`Temperature: ${temp}. The weather is moderate`); 
+}
+
+
+/* -------------------Exercise #2 ------------------------*/
+
