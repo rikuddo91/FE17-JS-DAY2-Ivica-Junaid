4078de690bc1bc33a9697b32efc1484fc10821ff
rikuddo91 <gullkhan0@live.com>
2022-09-22 13:58:19 +0200

    Basic_Finished

--- Basic/script/script.js
+++ Basic/script/script.js
@@ -1,6 +1,6 @@
-/* -------------------Exercise #1 ------------------------*/
+/* -------------------ex 1 | Temperature v1.0------------------------*/
 
-
+// Math.floor(Math.random() * (max + min) - min)
 let temp = Math.floor(Math.random() * (26 + 5) - 5)
 
 if(temp<11){
@@ -11,7 +11,7 @@ else {
 }
 
 
-/* -------------------Exercise #2 ------------------------*/
+/* -------------------ex 2 | Random Food------------------------*/
 
 const favoriteFood = ['burger', 'banana', 'baked cake', 'broccoli']
 
@@ -23,4 +23,58 @@ function randomFood(){
 randomFood();
 
 
-/* -------------------Exercise #3 ------------------------*/
+/* -------------------ex 3 | Crystal Gazer------------------------*/
+
+function crystalGlazer(){
+    let job = prompt("Enter job title: ");
+    let location = prompt("Enter location: ");
+    let partner = prompt("Enter job partner's name: ");
+    let children = prompt("Enter number of children: ");
+    console.log(`You will be a ${job} in ${location} and married to ${partner} with ${children} children.`)
+}
+crystalGlazer();
+
+/* -------------------ex 4 | Age Calculator------------------------*/
+
+function ageCalculator(){
+    let current_year = prompt("Enter current year ");
+    let birth_year = prompt("Enter your birth year ");
+    var age = current_year - birth_year;
+    var lower_age = age - 1;
+    console.log(`You are either ${age} or ${lower_age}`)
+}
+ageCalculator();
+
+/* -------------------ex 5 | Age Calculator-improved------------------------*/
+
+function ageCalculator(){
+    let current_year = new Date().getFullYear();
+    let birth_year = prompt("Enter your birth year ");
+    var age = current_year - birth_year;
+    var lower_age = age - 1;
+    console.log(`You are either ${age} or ${lower_age}`)
+}
+ageCalculator();
+
+/* ----------------ex 6 | Degree-Radian Conversion-------------------*/
+
+function degRadian(){
+    let degrees = prompt("Enter value in degrees ");
+    const pi = Math.PI; // BEWARE of small and capital Math.pi =/= Math.PI
+    var radians = degrees * (pi / 180);
+    console.log(`${radians} radians`);
+}
+degRadian();
+
+/* ----------------ex 7 | Area and Volume of a box-------------------*/
+
+
+
+function getBox (width, heihgt, depth){
+    let area = width*heihgt ;
+    let volume = width*heihgt*depth ;
+
+    return `The area of the box is: ${area}
+    The volume of the box is: ${volume}`;
+}
+console.log(getBox (2,7,5));
